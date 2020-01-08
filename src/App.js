9cb685504055ca5ea268f253abb9b465de9c1079
skobraf <skobraf@gmail.com>
2020-01-08 14:19:44 +0000

    complete algo stucture for start button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback, useRef} from 'react';
+import produce from 'immer';
 
+const numRows = 20;
+const numCols = 20;
+const operations = [
+  [0,1],
+  [0,-1],
+  [1,-1],
+  [-1,1],
+  [1,1],
+  [-1,-1],
+  [1,0],
+  [-1,0]
+]
 
-const numRows = 40;
-const numCols = 40;
 function App() {
   const [grid, setGrid] = useState(() => {
     const rows = [];
@@ -10,8 +21,51 @@ function App() {
       rows.push(Array.from(Array(numCols), () => 0))
     }
     return rows;
-  })
+  });
+    const [running, setRunning] = useState(false);
+    const runningRef = useRef(running);
+    runningRef.current = running;
+
+    const runSubmulation = useCallback(() => {
+      if(!runningRef.current) {
+        return;
+      }
+      setGrid(g => {
+        return produce(g, gridCopy => {
+          for(let i = 0; i < numRows; i++) {
+            for(let k = 0; k <numCols; k++) {
+              let neighbors = 0;
+              operations.forEach(([x,y]) => {
+                const newI = i + x;
+                const newK = k + y;
+                if(newI >= 0 && newI <numRows && newK >= 0 && newK < numCols) {
+                  neighbors += g[newI][newK]
+                }
+              })
+              if(neighbors < 2 || neighbors > 3) {
+                gridCopy[i][k] = 0;
+              }else if (g[i][k] === 0 && neighbors === 3) {
+                grid[i][k] = 1;
+              }
+            }
+          }
+        })
+      })
+      setTimeout(runSubmulation, 500);
+    },[])
   return (
+    <>
+    <button 
+      onClick={() => {
+        setRunning(!running);
+        if(!running) {
+          runningRef.current = true;
+          runSubmulation();
+        }
+      }}
+      >
+        {running ? "stop" : "start"}
+      </button>
     <div 
       style={{
         display: "grid",
@@ -23,7 +77,12 @@ function App() {
           rows.map((col, k) => (
             <div 
               key={`${i}-${k}`}
-              
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = gridCopy[i][k] ? 0 : 1;
+                })
+                setGrid(newGrid)
+              }}
               style={{
                 width:20,
                 height: 20,
@@ -34,6 +93,7 @@ function App() {
           ))
         )}
     </div>
+    </>
   );
 }
 
